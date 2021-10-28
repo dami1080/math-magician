cbb33b7e761fdfd8baf6063f712089767da60bdc
dami1080 <dareakintoye@gmail.com>
2021-10-27 16:36:49 -1100

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Calculator from './components/UI/Calculator';
 import Home from './pages/Home';
 import Quote from './pages/Quote';
+import Navbar from './components/UI/Navbar';
 
 class App extends React.Component {
   constructor(props) {
@@ -13,6 +14,7 @@ class App extends React.Component {
   render() {
     return (
       <fragment>
+        <Navbar />
         <Home />
         <div className="App">
           <Calculator />
---
+++ src/components/UI/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => (
+  <nav className="navbar">
+    <a href="/" className="nav-brand">Math Magicians</a>
+    <ul>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/calculator">Calculator</NavLink>
+      </li>
+      <li>
+        <NavLink to="/quote">Quote</NavLink>
+      </li>
+    </ul>
+  </nav>
+);
+export default Navbar;
