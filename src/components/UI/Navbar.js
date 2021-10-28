eb37c6fa480efecbed7bc97a032ca98f40c1bb65
dami1080 <dareakintoye@gmail.com>
2021-10-27 19:02:28 -1100

    style the complete site

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -7,13 +7,13 @@ const Navbar = () => (
     <a href="/" className="nav-brand">Math Magicians</a>
     <ul>
       <li>
-        <NavLink to="/">Home</NavLink>
+        <NavLink to="/" activeClassName="active-link" exact>Home</NavLink>
       </li>
       <li>
-        <NavLink to="/calculator">Calculator</NavLink>
+        <NavLink to="/calculator" activeClassName="active-link" exact>Calculator</NavLink>
       </li>
       <li>
-        <NavLink to="/quote">Quote</NavLink>
+        <NavLink to="/quote" activeClassName="active-link" exact>Quote</NavLink>
       </li>
     </ul>
   </nav>
