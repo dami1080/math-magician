a0d6596f0849112e5e189612e1a3683be4b0b48b
dami1080 <dareakintoye@gmail.com>
2021-10-27 18:29:32 -1100

    Add content and layout to the page

--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import './Quote.css';
 
 const Quote = () => (
-  <div>
-    Welcome to the quote page
+  <div className="quotes-page">
+    <div>
+      <p>
+        Mathematics knows no races or geographic boundaries; for mathematics,
+        the cultural world is one country.
+        <span>— David Hilbert</span>
+      </p>
+      <p>
+        Mathematics is not about numbers, equations, computations, or algorithms:
+        it is about understanding.
+        <span>— William Paul Thurston</span>
+      </p>
+    </div>
   </div>
 );
 
